18ce7a2ed587154454dcc66d9a06c6d2ad9f9ce6
Chris-Coleongco <christien.coleongco@gmail.com>
2024-07-26 09:16:07 -0600

    bleh

--- testing_ground/src/main.rs
+++ testing_ground/src/main.rs
@@ -1,25 +1,24 @@
-use pbkdf2::{pbkdf2_hmac};
+use pbkdf2::pbkdf2_hmac;
 use sha2::Sha256;
 use rand::RngCore;
+use std::fs::File;
+use std::io::Write;
 
 fn derive_key(password: &[u8], salt: &[u8]) -> [u8; 32] {
     let mut key = [0u8; 32];
     pbkdf2_hmac::<Sha256>(password, salt, 100_000, &mut key);
-    return key
+    return key;
 }
 
 
 fn generate_salt() -> [u8; 16] {
     let mut salt = [0u8; 16];
     rand::thread_rng().fill_bytes(&mut salt);
-    salt
+    return salt;
 }
 
 
-
-
-
-fn main() {
+fn authenticate() -> String {
 
     let mut master_password = String::new();
 
@@ -27,19 +26,40 @@ fn main() {
 
     println!("{}", master_password);
 
-    let key = derive_key(master_password.trim().as_bytes(), &generate_salt());
+    return master_password;
+
+
+}
+
+
+
+fn add_entry(data_name: String, data: String, password: String) {
+
+    let salt = &generate_salt();
+
+    let key = derive_key(password.trim().as_bytes(), salt);
+
+    println!("salt: {:?}", salt);
 
     println!("{:?}", key);
 
-    save the salt to a file in the same dir as the password. both files will have the same name but differing extensions.
+  //  save the salt as the second line after the password
 
-    the salt will be unique. one for each password to be stored encrypted
+    let mut file = File::create(data_name).expect("couldn't create file");
+  //  the salt will be unique. one for each password to be stored encrypted
 
-    password1.txt
+    file.write_all(&key).expect("Unable to write data");
 
-    password1.salt
+  //  encrypt with 256 aes (key derived is 32 bytes aka 32 bytes = 8 bits/byte * 32 bytes = 256 bits)
 
-    encrypt with 256 aes (key derived is 32 bytes aka 32 bytes = 8 bits/byte * 32 bytes = 256 bits)
 
+
+}
+
+fn main() {
+
+    let master_password = authenticate();
+
+    
 
 }
