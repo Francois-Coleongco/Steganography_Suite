0669ed99ea6770824a669247e11a06db5e3134d5
Chris-Coleongco <christien.coleongco@gmail.com>
2024-07-29 08:08:18 -0600

    added handler, also why did i set it so the key is the one that gets saved to a file TwT anywayas i fixed that its now the salt that gets saved. now all i need to do is use the key to encrypt the data via aes and then have my program in the read entry functionality derive the key again using the salt and given password to decrypt the data. symmetric encryption

--- testing_ground/src/main.rs
+++ testing_ground/src/main.rs
@@ -6,7 +6,7 @@ use std::io::Write;
 
 fn derive_key(password: &[u8], salt: &[u8]) -> [u8; 32] {
     let mut key = [0u8; 32];
-    pbkdf2_hmac::<Sha256>(password, salt, 100_000, &mut key);
+    pbkdf2_hmac::<Sha256>(password, salt, 600_000, &mut key);
     return key;
 }
 
@@ -21,7 +21,11 @@ fn generate_salt() -> [u8; 16] {
 fn authenticate() -> String {
 
     let mut master_password = String::new();
+    
+    print!("enter master password: ");
 
+    std::io::stdout().flush().expect("couldn't flush 3");
+    
     std::io::stdin().read_line(&mut master_password).expect("couldn't read input");
 
     println!("{}", master_password);
@@ -34,6 +38,8 @@ fn authenticate() -> String {
 
 
 fn add_entry(data_name: String, data: String, password: String) {
+
+    println!("data: {}", data);
 
     let salt = &generate_salt();
 
@@ -48,7 +54,7 @@ fn add_entry(data_name: String, data: String, password: String) {
     let mut file = File::create(data_name).expect("couldn't create file");
   //  the salt will be unique. one for each password to be stored encrypted
 
-    file.write_all(&key).expect("Unable to write data");
+    file.write_all(salt).expect("Unable to write data");
 
   //  encrypt with 256 aes (key derived is 32 bytes aka 32 bytes = 8 bits/byte * 32 bytes = 256 bits)
 
@@ -56,10 +62,49 @@ fn add_entry(data_name: String, data: String, password: String) {
 
 }
 
-fn main() {
-
+fn add_entry_handler() {
+    
     let master_password = authenticate();
 
+    let mut data_name = String::new();
+
+    let mut data = String::new();
+
+    print!("name your entry: ");
+
+    std::io::stdout().flush().expect("couldn't flush 1");
+
+    std::io::stdin().read_line(&mut data_name).expect("couldn't read input for entry name");
+
+    print!("\nprovide data: ");
     
+    std::io::stdout().flush().expect("couldn't flush 2");
+    
+    std::io::stdin().read_line(&mut data).expect("couldn't read input for entry data");
+
+    println!("\n ---- o ---- o ---- o ---- \n adding entry");
+
+    add_entry(data_name, data, master_password);
+
+}
+
+fn main() {
+
+    println!("welcome to stego-rs\nplease select an option:\n\n1 = add entry\n2 = read entry\n3 = delete entry");
+
+    let mut option = String::new();
+
+    std::io::stdin().read_line(&mut option).expect("couldn't read selected option");
+
+    match option.trim() {
+        "1" => add_entry_handler(),
+        "2" => println!("read entry | selected"),
+        "3" => println!("delete entry | selected"),
+        _ => println!("that isn't an option")
+    }
+
+    
+
+
 
 }
